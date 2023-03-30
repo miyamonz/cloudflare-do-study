84dd0c5862dd9bc431a770daa892c66ed679767c
miyamonz <m5813k@gmail.com>
2023-03-31 02:33:16 +0900

    tmp

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { HelloDurable } from "./HelloDurable";
-
+export { ChatRoom } from "../client/server/ChatRoom";
 export interface Env {
   HELLO_DURABLE: DurableObjectNamespace;
 }
@@ -12,11 +12,12 @@ export default {
   ): Promise<Response> {
     const url = new URL(request.url);
     const path = url.pathname.slice(1);
+    console.log(env);
     if (path === "hello_durable") {
       //   const id = env.HELLO_DURABLE.newUniqueId();
       const id = env.HELLO_DURABLE.idFromName("miyamonz");
       const stub = env.HELLO_DURABLE.get(id);
-      const response = await stub.fetch("http://do.fake/");
+      const response = await stub.fetch("http://example.com/");
       const text = await response.text();
       return new Response(text);
     }
