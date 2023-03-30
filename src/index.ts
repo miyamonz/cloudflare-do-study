f2a7a19a32b430977536bfa98ab99d1194712332
miyamonz <m5813k@gmail.com>
2023-03-30 23:48:50 +0900

    durable objects

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export { HelloDurable } from "./HelloDurable";
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
+  HELLO_DURABLE: DurableObjectNamespace;
 }
 
 export default {
@@ -28,6 +10,16 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+    const url = new URL(request.url);
+    const path = url.pathname.slice(1);
+    if (path === "hello_durable") {
+      //   const id = env.HELLO_DURABLE.newUniqueId();
+      const id = env.HELLO_DURABLE.idFromName("miyamonz");
+      const stub = env.HELLO_DURABLE.get(id);
+      const response = await stub.fetch("http://do.fake/");
+      const text = await response.text();
+      return new Response(text);
+    }
+    return new Response("Hello from Worker!");
   },
 };
